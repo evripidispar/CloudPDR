3e92a63f842996aef916548f2332b06d5139682d
vlekakis <lekakis@gmail.com>
2014-05-15 13:46:31 -0400

    fix load from disk

--- src/driver.py
+++ src/driver.py
@@ -498,7 +498,7 @@ def main():
     
     pool = []
     for i in xrange(args.workers):
-        p = mp.Process(target=workerTask, args=(blockByteChunks,W,T,ibf,fs.pbSize,fs.datSize,secret,public, TT))
+        p = mp.Process(target=workerTask, args=(blockByteChunks,W,T,ibf,fs.pbSize,fs.datSize,secret,public, TT, doNotComputeTags))
         p.start()
         pool.append(p)
     
@@ -540,8 +540,8 @@ def main():
                                                T, cltId, args.hashNum, delta, fs.numBlk, args.runId)
 
 
-    ip = '192.168.1.14'
-    #ip = "127.0.0.1"
+    #ip = '192.168.1.14'
+    ip = "127.0.0.1"
     zmqContext =  zmq.Context()
     clt = RpcPdrClient(zmqContext)    
     print "Sending Initialization message"
